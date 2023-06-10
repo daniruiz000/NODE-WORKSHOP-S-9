36de0ebcb442adb6c0b881ecebaa94efbaf2ebef
abj1389 <abj_13@hotmail.com>
2023-06-10 14:05:49 +0200

    impruve order service

--- src/domain/services/order.service.ts
+++ src/domain/services/order.service.ts
@@ -1,5 +1,7 @@
 import { Response, NextFunction } from "express";
 import { orderOdm } from "../odm/order.odm";
+import { AllowedStatus } from "../entities/order-entity";
+import { ICake } from "../entities/cake-entity";
 
 export const getOrdersByUser = async (req: any, res: Response, next: NextFunction): Promise<void> => {
   try {
@@ -86,7 +88,9 @@ export const createOrder = async (req: any, res: Response, next: NextFunction): 
       return;
     }
     const temporalOrder = {
-      ...req.body,
+      cakes: req.body.cakes,
+      status: "CART",
+      date: new Date(),
       user: req.user.id,
     };
     const createdOrder = await orderOdm.createOrder(temporalOrder);
@@ -120,20 +124,46 @@ export const updateOrder = async (req: any, res: Response, next: NextFunction): 
   try {
     const id = req.params.id;
 
-    if (req.user.id !== id && req.user.email !== "admin@gmail.com") {
+    const orderToUpdate = await orderOdm.getOrderById(id);
+    if (!orderToUpdate) {
+      res.status(404).json({ error: "Pedido no encontrado." });
+      return;
+    }
+    const address: string = req.body.address;
+    const cakes: ICake[] = req.body.cakes;
+    const status: AllowedStatus = req.body.status;
+
+    if (req.user.id !== orderToUpdate.toObject().user && req.user.email !== "admin@gmail.com") {
       res.status(401).json({ error: "No tienes autorización para realizar esta operación" });
       return;
     }
 
-    const orderToUpdate = await orderOdm.getOrderById(id);
-    if (orderToUpdate) {
-      Object.assign(orderToUpdate, req.body);
-      await orderToUpdate.save();
-      const orderToSend: any = orderToUpdate.toObject();
-      res.json(orderToSend);
+    if (req.user.email === "admin@gmail.com") {
+      Object.assign(orderToUpdate, {
+        status,
+        address,
+        cakes,
+        date: orderToUpdate.toObject().status !== status && status === AllowedStatus.PENDING ? new Date() : orderToUpdate.toObject().date,
+      });
     } else {
-      res.status(404).json({ error: "Pedido no encontrado." });
+      if (orderToUpdate.toObject().status !== AllowedStatus.CART || (status !== AllowedStatus.PENDING && status !== AllowedStatus.CART)) {
+        res.status(401).json({ error: "No tienes autorización para realizar esta operación" });
+        return;
+      }
+      if (!cakes.length && status === AllowedStatus.PENDING) {
+        res.status(400).json({ error: "No hay tartas en el pedido, melón!" });
+        return;
+      }
+      Object.assign(orderToUpdate, {
+        status,
+        address,
+        cakes,
+        date: orderToUpdate.toObject().status !== status && status === AllowedStatus.PENDING ? new Date() : orderToUpdate.toObject().date,
+      });
     }
+
+    await orderToUpdate.save();
+    res.json(orderToUpdate);
   } catch (error) {
     next(error);
   }
